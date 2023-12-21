0f06fe265f622deef1b98072b68dfcfd05a40f06
Mike Utz <mike.u@delegator.com>
2023-12-21 12:38:00 +0300

    feat(tf): setup github actions environment variables
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- terraform/github.tf
+++ terraform/github.tf
@@ -6,3 +6,17 @@ resource "github_repository_environment" "prd" {
   environment = "production"
   repository  = github_repository.this.name
 }
+
+resource "github_actions_environment_variable" "hubspot_portal_id" {
+  repository    = github_repository.this.name
+  environment   = github_repository_environment.prd.environment
+  variable_name = "hubspot_portal_id"
+  value         = "44731386"
+}
+
+resource "github_actions_environment_secret" "hubspot_personal_access_key" {
+  repository      = github_repository.this.name
+  environment     = github_repository_environment.prd.environment
+  secret_name     = "hubspot_personal_access_key"
+  plaintext_value = data.doppler_secrets.prd.map.HUBSPOT_PERSONAL_ACCESS_KEY
+}
