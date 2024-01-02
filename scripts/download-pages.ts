d766b290d1a21b7ba428fbbef9f879661068276a
Mike Utz <mike.u@delegator.com>
2024-01-02 11:50:43 +0300

    refactor(scripts): rename client
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- scripts/download-pages.ts
+++ scripts/download-pages.ts
@@ -1,21 +1,9 @@
 import { Page } from "@hubspot/api-client/lib/codegen/cms/pages";
 import { writeFile } from 'fs/promises'
 import { pagesDumpFile } from './utils/const'
-import { makeClient as makeHubspotClient } from "./utils/hubspot";
+import { makeClient as makeHubspotClient } from "./utils/hubspotClient.ts";
 
 const devClient = await makeHubspotClient("dev")
-
-const createdAt = undefined;
-const createdAfter = undefined;
-const createdBefore = undefined;
-const updatedAt = undefined;
-const updatedAfter = undefined;
-const updatedBefore = undefined;
-const sort = undefined;
-const after = undefined;
-const limit = undefined;
-const archived = undefined;
-const property = undefined;
 
 let pages: Array<Page>;
 
@@ -23,24 +11,12 @@ try {
   let msg = "Downloading pages from HubSpot..."
   console.log(msg)
 
-  const downRes = await devClient.cms.pages.sitePagesApi.getPage(
-    createdAt,
-    createdAfter,
-    createdBefore,
-    updatedAt,
-    updatedAfter,
-    updatedBefore,
-    sort,
-    after,
-    limit,
-    archived,
-    property
-  );
+  const res = await devClient.cms.pages.sitePagesApi.getPage();
 
   console.log(`${msg}DONE`)
-  console.log(`Total pages downloaded: ${downRes.total}`)
+  console.log(`Total pages downloaded: ${res.total}`)
 
-  pages = downRes.results;
+  pages = res.results;
 } catch (e: unknown) {
   if (e instanceof Error) {
     console.error(e.message)
--- scripts/utils/hubspotClient.ts
+++ scripts/utils/hubspotClient.ts
@@ -5,8 +5,7 @@ export async function makeClient(dopplerEnv: "dev" | "prd"): Promise<Client> {
   const accessToken = await getSecret(dopplerEnv, "HUBSPOT_PRIVATE_APP_TOKEN")
 
   if (!accessToken) {
-    console.error(`HubSpot access token not found for env: ${dopplerEnv}.`)
-    process.exit(1)
+    throw new Error(`HubSpot access token not found for env: ${dopplerEnv}.`);
   }
 
   const client = new Client({ accessToken });
