a87dacdfff958b6cdfb79dceef36b38c3a460fad
Mike Utz <mike.u@delegator.com>
2024-01-02 12:22:01 +0300

    refactor(scripts): cleanup
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- scripts/utils/hubspotClient.ts
+++ scripts/utils/hubspotClient.ts
@@ -1,19 +1,13 @@
 import { Client } from "@hubspot/api-client"
 import { getSecret } from "./doppler";
 
-type env = "dev" | "prd";
+type Envs = "dev" | "prd";
 
-export function isEnv(env: string): env is env {
-  switch (env) {
-    case "dev":
-    case "prd":
-      return true;
-    default:
-      return false;
-  }
+export function isEnv(env: string): env is Envs {
+  return ["dev", "prd"].includes(env)
 }
 
-export async function makeClient(dopplerEnv: "dev" | "prd"): Promise<Client> {
+export async function makeClient(dopplerEnv: Envs): Promise<Client> {
   const accessToken = await getSecret(dopplerEnv, "HUBSPOT_PRIVATE_APP_TOKEN")
 
   if (!accessToken) {
